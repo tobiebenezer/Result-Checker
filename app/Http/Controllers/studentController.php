80e2c3681fe34823c53308d5d6326d28f1bd238b
Tobi Ebenezer <tobisamuel70@gmail.com>
2022-04-08 11:42:30 +0100

    incomplete student controller

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -6,6 +6,7 @@ use App\Models\courses;
 use App\Models\entity;
 use App\Models\results;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class studentController extends Controller
 {
@@ -17,8 +18,12 @@ class studentController extends Controller
     public function resultView(Request $request){
         $matric_number = entity::select('matric_no')->where('id',Auth::user()->role)->first()->matric_no;
 
-        $result = results::select()->where('matric_no',$matric_number)->get();
+        // $result = results::select()->where('matric_no',$matric_number)->get();
 
-        $course = courses::select()->get()
+        // $course = courses::select()->get()
+        $user = DB::table("courses")
+                        ->join('results','courses.id','=','results.course_id')->select('courses.course_name','courses.level','course_name.credit_load','results.grade','results.session','results.matric_no')->where('results.matric
+                        ');
+
     }
 }
